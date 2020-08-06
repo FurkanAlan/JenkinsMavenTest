a30285605229c55bd562037086fb05f5512180e3
FurkanAlan <ulkirr@gmail.com>
2020-08-06 08:59:50 -0500

    second message added

--- src/test/java/test/HelloJenkins.java
+++ src/test/java/test/HelloJenkins.java
@@ -3,5 +3,6 @@ package test;
 public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins");
+        System.out.println("Second Test");
     }
 }
