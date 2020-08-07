594a5f0b5da166c9a4d0d300a3f88cc82bcd3969
FurkanAlan <64380664+FurkanAlan@users.noreply.github.com>
2020-08-06 22:55:14 -0500

    Update HelloJenkins.java

--- src/test/java/test/HelloJenkins.java
+++ src/test/java/test/HelloJenkins.java
@@ -5,5 +5,7 @@ public class HelloJenkins {
         System.out.println("Hello Jenkins");
         System.out.println("Second Test");
                 System.out.println("Third Test");
+                        System.out.println("fourth Test");
+
     }
 }
