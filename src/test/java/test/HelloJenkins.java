bfb7871d1984ee772577cf7bf4a11e6033225920
FurkanAlan <64380664+FurkanAlan@users.noreply.github.com>
2020-08-07 18:48:06 -0500

    Update HelloJenkins.java

--- src/test/java/test/HelloJenkins.java
+++ src/test/java/test/HelloJenkins.java
@@ -3,7 +3,7 @@ package test;
 public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins");
-        System.out.println("Second Test");
+        System.out.println("Second");
                 System.out.println("Third");
                         System.out.println("fourth");
 
